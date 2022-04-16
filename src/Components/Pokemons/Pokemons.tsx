7cf2c05b65981c62c2a318a75a7ffd915b350610
Armando Almanza <armando.s.almanza@gmail.com>
2022-04-15 22:17:40 -0500

    testing some thing

--- src/Components/Pokemons/Pokemons.tsx
+++ src/Components/Pokemons/Pokemons.tsx
@@ -3,22 +3,34 @@ import { useState } from "react";
 const Pokemon = () => {
     const url = "https://pokeapi.co/api/v2/pokemon/umbreon/";
 
-    const [pokemon, setPokemon] = useState("pokamion aleatorio");
+    const [pokemon, setPokemon] = useState({
+        id: 0,
+        name: "",
+        sprite: "",
+        baseExperience: "",
+        abilities: [""],
+    });
     const [pokemonImage, setPokemonImage] = useState("pokamion aleatorio");
-    const [ability, setAbility] = useState([]);
 
     const getPokamion = async () => {
         await fetch(url)
             .then((res) => res.json())
             .then((data) => {
-                const { name, abilities, sprites } = data;
-                setPokemonImage(sprites.front_default);
-                getHabilities(abilities);
-                setPokemon(name);
+                const { name, abilities, sprites, id, base_experience } = data;
+                const { front_default } = sprites;
+                setPokemon(
+                    ((pokemon.name = name),
+                    (pokemon.id = id),
+                    ((pokemon.sprite = front_default),
+                    (pokemon.baseExperience = base_experience),
+                    (pokemon.abilities = abilities)))
+                );
+                console.log(pokemon);
+                setPokemonImage(front_default);
             });
     };
 
-    const getHabilities = (habilities: [any]) => {
+    /* const getHabilities = (habilities: [any]) => {
         habilities.map((habilitie) => {
             const { name } = habilitie.ability;
             console.log(name);
@@ -29,14 +41,12 @@ const Pokemon = () => {
     function NumberList(props: string[]) {
         const listItems = props.map((abi) => <li key={ability.length}>{abi}</li>);
         return <ul>{listItems}</ul>;
-    }
+    } */
 
     return (
         <div>
             <h1>Pokamions</h1>
             <button onClick={getPokamion}>Get Pokamion</button>
-            <p>{pokemon}</p>
-            {NumberList(ability)}
             <img src={pokemonImage} alt="pokamion random" />
         </div>
     );
